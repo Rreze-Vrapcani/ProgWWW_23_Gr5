ed65bf4e75e81ad8d6305963b2848e87fd4b99e6
eradedinca <122313196+eradedinca@users.noreply.github.com>
2024-01-09 14:50:25 +0100

    added image slide show

---
+++ js/roomDetails.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', () => {
+    let thumbnails = document.querySelectorAll('.thumbnail');
+    let mainImage = document.getElementById('main-image').querySelector('img');
+  
+    thumbnails.forEach(thumbnail => {
+      thumbnail.addEventListener('click', function() {
+        mainImage.src = this.src;
+      });
+    });
+  
+    // Implement swipe functionality (basic example)
+    let startTouch = null;
+  
+    document.getElementById('thumbnail-slider').addEventListener('touchstart', function(e) {
+      startTouch = e.touches[0].clientX;
+    });
+  
+    document.getElementById('thumbnail-slider').addEventListener('touchmove', function(e) {
+      if (!startTouch) return;
+  
+      let touch = e.touches[0].clientX;
+      let change = startTouch - touch;
+  
+      if (change > 0) {
+        this.scrollLeft += 10;
+      } else {
+        this.scrollLeft -= 10;
+      }
+    });
+  
+    document.getElementById('thumbnail-slider').addEventListener('touchend', function(e) {
+      startTouch = null;
+    });
+  });
+  
